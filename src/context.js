c6d27b89ef8a3a66a0b3bd7cffa425069716c7be
ebetron <ebetron@gmail.com>
2019-06-13 18:20:27 -0400

    Plugin data to context and setup intial state

--- src/context.js
+++ src/context.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
+import {storeProducts, detailProduct} from './data';
 
 const ProductContext = React.createContext();
 //Provider - provides all info for application, sits on top of application
 //Consumer - use this object to "consume" info from provider
 
 class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        details: detailProduct
+    }
     render(){
         return(
-            <ProductContext.Provider value="hello from phone app">
+            <ProductContext.Provider value={{...this.state}}>
                 {this.props.children}
             </ProductContext.Provider>
         )
