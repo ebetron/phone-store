ad03f1d9bc9eda6b2d05294bfc8ab30088ec6a65
ebetron <ebetron@gmail.com>
2019-06-12 21:31:53 -0400

    Navbar Component - Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import logo from "./logo.svg";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">Column Number One</div>
-        <div className="col-6"><span><i className="fas fa-home"/></span>Column Number Two</div>
-      </div>
-    </div>
+    <React.Fragment>
+      <h3>Hello From App</h3>
+    </React.Fragment>
   );
 }
 
