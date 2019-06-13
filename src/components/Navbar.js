406f8e2ce14e2179b2fde8cd808c7da57cf6ab73
ebetron <ebetron@gmail.com>
2019-06-13 17:45:34 -0400

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import  React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
 import {ButtonContainer} from './Button';
 
 
 export default class Navbar extends Component {
     render(){
         return(
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                             {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
@@ -16,7 +17,7 @@ https://www.iconfinder.com/Makoto_msk */}
                 </Link>
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">Products</Link>
+                        <Link to="/" className="nav-link">products</Link>
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
@@ -27,7 +28,16 @@ https://www.iconfinder.com/Makoto_msk */}
                         my cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+`
